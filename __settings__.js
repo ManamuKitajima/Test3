f863d4c6f483818bf22208f494f01e88419b3b1a
ManamuKitajima <50942523+ManamuKitajima@users.noreply.github.com>
2021-12-17 12:17:25 +0900

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1292349.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 62895209, 62896543, 62896628, 62905590 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/62896158/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/62896159/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/62896157/1/ammo.js', 'preload' : true},
+];
